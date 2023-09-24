4c6d7178c2c4255c268377bdb4fb4cb7721f9b89
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-09-24 20:32:14 +0300

    Add the rack-cors gem & install it

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -14,3 +14,14 @@
 #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
 #   end
 # end
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins '*'  # Ideally, replace '*' with the actual origin of your React app for security
+      resource '*', 
+      resource "*",
+      headers: :any,
+      methods: [:get, :head]    
+    end
+ end
+ 
