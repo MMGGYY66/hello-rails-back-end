b48f36e98ca2b06ba407d587deffb9985da91901
MOHAMED GAMIL ELDIMARDASH <gigiyoyo2001@yahoo.com>
2023-09-25 02:14:17 +0300

    update greetings controller

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,6 +1,6 @@
 class GreetingsController < ApplicationController
   def random
-    greeting = Greeting.order(Arel.sql('RANDOM()')).first
-    render json: { greeting: greeting.message }
+    greeting = Greeting.order('RANDOM()').first
+    render json: { message: greeting&.message || 'No greetings available.' }
   end
 end
